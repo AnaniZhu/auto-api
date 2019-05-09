baf6ac3419292c9fc7819cdcfea7a51aaf02126c
zhuwenhan <zhuwenhan@dxy.cn>
2019-05-09 15:05:42 +0800

    feat: 调整不同的传递参数泛型

--- .automock.js
+++ .automock.js
@@ -2,15 +2,28 @@ const injectRequestFileText = `
 function createServer(method, mockUrl, prodUrl, isMock): Function {
   return () => {};
 }
-
 interface IBase<T> {
-  slience?: boolean;
-  query?: T;
+  slience?: boolean
+  params?: T // get/post/put query
 }
 
-interface IParams<T, R> extends IBase<T> {
-  params: R;
+// 动态参数
+interface IPayload<T, K> extends IBase<T> {
+  payload: K
 }
+
+// post/put/patch 放在 hDttp body 里的数据
+interface IData<T, R> extends IBase<T> {
+  data: R
+}
+
+// payload 和 data 都有
+
+interface IBoth<T, K, R> extends IBase<T> {
+  payload: K
+  data: R
+}
+
 `
 
 
@@ -19,16 +32,22 @@ module.exports = {
   tabSize: 2,
   // interface 名前缀
   interfacePrefix: 'I',
-  // params 的 interface 名后缀
-  paramsInterfaceSuffix: 'Params',
-  // payload 的 interface 名后缀
-  payloadInterfaceSuffix: 'Payload',
+  // 路由 动态id 的 interface 名后缀
+  dynamicPathInterfaceSuffix: 'Payload',
+  // get 请求所带参数 的 interface 名后缀
+  queryInterfaceSuffix: 'Params',
+  // post/delete/put/patch 所带数据（body）
+  bodyInterfaceSuffix: 'Data',
   // response 的 interface 名后缀
   responseInterfaceSuffix: 'Response',
   // 继承的基础 interface 名
   baseExtendInterface: 'IBase',
-  // 继承的拥有 params（动态路径） interface 名
-  paramsExtendInterface: 'IParams',
+  // 继承的拥有 动态路径 interface 名
+  dynamicPathExtendInterface: 'IPayload',
+  // post/put/patch 传入参数在 http body 中的 interface 名
+  bodyExtendInterface: 'IData',
+  // 以上两者同时满足时，采用的 继承 interface
+  bothExtendInterface: 'IBoth',
   // 创建 request 的方法名
   createRequestFn: 'createServer',
   // 输出 interface 的路径
@@ -43,14 +62,14 @@ module.exports = {
   injectRequestFileText,
   // 需要订阅的模块
   subscribeMockModules : [
-    // '5b1e5036e1540c84e3377e88',
-    {
-      hash: '5b1e5036e1540c84e3377e88',
-      name: 'MyPatient',
-      includes: [
-        '5c37fbd13fef450c3d2f7219',
-        '5c1b64d3cd2da96e80a27cc1'
-      ]
-    }
+    '5b1e5036e1540c84e3377e88',
+    // {
+    //   hash: '5b1e5036e1540c84e3377e88',
+    //   name: 'MyPatient',
+    //   includes: [
+    //     '5c37fbd13fef450c3d2f7219',
+    //     '5c1b64d3cd2da96e80a27cc1'
+    //   ]
+    // }
   ]
 }
